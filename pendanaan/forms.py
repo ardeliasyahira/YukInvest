b5ef57acf5acb2362a4388018551c3a060244689
Nugraha.akt <Nugraha.akt@gmail.com>
2021-10-29 09:34:05 +0700

    Menambah template pendanaan

--- pendanaan/forms.py
+++ pendanaan/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django.forms.widgets import Input, Select, Textarea
 from .models import UMKM
 
 class UMKMForm(ModelForm):
@@ -8,6 +9,16 @@ class UMKMForm(ModelForm):
 	    'merek_bisnis', 'domisili', 'produk_jasa', 'pendanaan_dibutuhkan', 'deskripsi',
 	    'logo_usaha', 'gambar_usaha', 'ringkasan_perusahaan'
 	]
+        widgets = {
+            "merek_bisnis": Input(attrs={"class": "form-control"}),
+            "domisili": Select(attrs={"class": "form-control"}, choices=(UMKM.PILIHAN_DOMISILI)),
+            "produk_jasa": Input(attrs={"class": "form-control"}),
+            "pendanaan_dibutuhkan": Input(attrs={"type": "number", "class": "form-control"}),
+            "deskripsi": Textarea(attrs={"class": "form-control"}),
+            "logo_usaha": Input(attrs={"type": "file", "class": "form-control"}),
+            "gambar_usaha": Input(attrs={"type": "file", "class": "form-control"}),
+            "ringkasan_perusahaan": Input(attrs={"type": "file", "class": "form-control"}),	    
+        }
 
 
   
---
+++ pendanaan/views.py
@@ -0,0 +1,39 @@
+from typing import final
+from django.shortcuts import redirect, render
+from pendanaan.forms import UMKMForm
+from .models import UMKM
+from django.core import serializers
+from django.http.response import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+def index(request):
+    return render(request, "pendanaan/index_pendanaan.html")
+
+# @login_required(login_url="")
+def get_umkm(request):
+    umkm = UMKM.objects.all()
+    umkm_json = serializers.serialize("json", umkm)
+    return HttpResponse(umkm_json, content_type="application/json")
+
+# @login_required(login_url="")
+def add_umkm(request):
+    form = UMKMForm()
+    if request.method == "POST":
+        data = request.POST.dict()
+        # data["user"] = request.user
+        form = UMKMForm(data, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("") #
+    context = {"form": form}
+    return render(request, "pendanaan/add_umkm.html", context)
+
+
+# @login_required(login_url="")
+def delete_umkm(request, umkm_id):
+    try:
+        UMKM.objects.get(id=umkm_id).delete()
+    except Exception as e:
+        print(e)
+    finally:
+        return redirect("") #
