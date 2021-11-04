d73b0c2543cd45e26902fdcd95222ff59de67cb7
Nurul Hanina Efendi <nurul.hanina@ui.ac.id>
2021-11-04 10:13:03 +0700

    rapihin code

--- pasar_saham/views.py
+++ pasar_saham/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render, HttpResponseRedirect, HttpResponse, redirect
-from django.http import JsonResponse
+from django.shortcuts import render
 from pendanaan.models import UMKM
 from django.template.loader import render_to_string
 from django.http import JsonResponse
-from django.db.models import Q
-from .forms import SearchForm
 
 
 # Create your views here.
@@ -31,23 +28,3 @@ def detail(request, pk):
     umkm = UMKM.objects.get(merek_bisnis=pk)
     return render(request, 'detail.html', {'detail_umkm': umkm})
 
-# def search(request):
-#     if 'search' in request.GET:
-#         value = request.GET['search']
-#         umkm = UMKM.objects.filter(merek_usaha__startswith=value)
-#     else:
-#         umkm = UMKM.objects.all()
-#
-#     html = render_to_string(template_name='index.html', context={'umkm': umkm})
-#     value_dict = {'html_from_view': html}
-#     return JsonResponse(data=value_dict, safe=False)
-
-
-def search(request):
-    searched = request.POST['searched']
-    umkm = UMKM.objects.filter(Q(merek_usaha__contains=searched) | Q(domisili__contains=searched)).order_by('merek_usaha')
-
-    if request.method == "POST":
-        return render(request, 'search.html', {'searched': searched, 'umkm': umkm})
-    else:
-        return render(request, 'search.html', {})
