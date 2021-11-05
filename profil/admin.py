d7885c71d088167d7e2084952735290c1458d04e
ardelia.syahira <ardelia.syahira@ui.ac.id>
2021-11-05 18:02:33 +0700

    update html tapi datanya ttp gabisa masuk hue

--- profil/admin.py
+++ profil/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EditProfil
+# from .models import EditProfil
 from profil.forms import moform
 from profil import models
 # Register your models here.
